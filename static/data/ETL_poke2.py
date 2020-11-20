f5273af836924084ec8be913f24590a4f45a518d
John Whitaker <johnwhitakeraustin@gmail.com>
2020-11-20 09:08:19 -0800

    Updated ETL to array to render table

--- static/data/ETL_poke2.py
+++ static/data/ETL_poke2.py
@@ -9,7 +9,8 @@ pokemon = pokemon.drop(columns=["Type 2"])
 gen_list = pokemon.loc[:,"Generation"].unique()
 pokedex_fields = ["name","type1","total","hp","attack","defense","spatk","spdef","speed","generation"]
 pokedex_dict = {}
-wip_dict_gen = {}
+#wip_dict_gen = {}
+wip_list_gen = []
 wip_dict_poke = {}
 # Subset Legendary Pokemon
 legends = pokemon.loc[(pokemon["Legendary"]=="Yes")&(pokemon["Mega"]=="No")].reset_index()
@@ -26,10 +27,12 @@ for i in gen_list:
                 wip_dict_poke[pokedex_fields[k]]=(wip_df.iloc[j,k])
             else:
                 wip_dict_poke[pokedex_fields[k]]=(wip_df.iloc[j,k].item())
-        wip_dict_gen[wip_df.iloc[j,0]] = (wip_dict_poke)
+        #wip_dict_gen[wip_df.iloc[j,0]] = (wip_dict_poke)
+        wip_list_gen.append(wip_dict_poke)
         wip_dict_poke = {}
-    pokedex_dict[f"gen{i}"]=(wip_dict_gen)
-    wip_dict_gen = {}
+    pokedex_dict[f"gen{i}"]=(wip_list_gen)
+    #wip_dict_gen = {}
+    wip_list_gen = []
 # Add Legendary Pokemon dict to Pokedex
 for i in range(len(legends)):
     for j in range(len(pokedex_fields)):
@@ -37,10 +40,27 @@ for i in range(len(legends)):
             wip_dict_poke[pokedex_fields[j]]=(legends.iloc[i,j])
         else:
             wip_dict_poke[pokedex_fields[j]]=(legends.iloc[i,j].item())
-    wip_dict_gen[legends.iloc[i,0]] = (wip_dict_poke)
+    #wip_dict_gen[legends.iloc[i,0]] = (wip_dict_poke)
+    wip_list_gen.append(wip_dict_poke)
     wip_dict_poke = {}
-pokedex_dict["legendary"] = (wip_dict_gen)
-wip_dict_gen = {}
+pokedex_dict["genL"] = (wip_list_gen)
+#wip_dict_gen = {}
+wip_list_gen = []
+# Add dict with Pokemon names for each gen
+gen_name_lists = {}
+wip_list = []
+for i in gen_list:
+    gen_name = f"gen{i}_names"
+    gen = f"gen{str(i)}"
+    for j in pokedex_dict[gen]:
+        wip_list.append(j)
+    gen_name_lists[gen_name] = (wip_list)
+    wip_list = []
+for i in pokedex_dict["genL"]:
+    wip_list.append(i)
+gen_name_lists["genL"] = (wip_list)
+pokedex_dict["gen_name_lists"] = (gen_name_lists)
+wip_list = []
 ### Combat Vars ---------------------------------
 # Read Data
 type_matchups = pd.read_csv("type_matchups.csv").set_index("TYPE")
--- static/js/match_active.js
+++ static/js/match_active.js
@@ -1,29 +1,51 @@
 // Remebering that the easier way is to create a function then just pass in the data to
 // hopefully avoid some of the bs I'm dealing with here
 
+function roster_table(pokedex,match_vars){
+    // var gen = "gen" + match_vars[0].gen
+    // var gen_names = gen + "_names"
+    // var roster = pokedex[0][gen].Charizard
+    // var roster_names = pokedex[0].gen_name_lists[gen_names]
+    // console.log(roster.name)
+    console.log(pokedex[0].gen1)
+    d3.select("tbody")
+        .selectAll("tr")
+        .data(pokedex[0].gen1)
+        .enter()
+        .append("tr")
+        .html(d => `<td>${d.name}</td>
+            <td>${d.type1}</td>`);
+}
 
-d3.json("pokedex").then(pokemon=>
-    d3.json("match_vars").then(vars=>
-        a = vars[0].gen,
-        console.log(a),
-        poke = pokemon[0]["gen1"],
-        d3.select("tbody")
-            .selectAll("tr")
-            .data(poke)
-            .enter()
-            .append("tr")
-            .html(poke => `<td>${poke.name}</td>
-                <td>${poke.type1}</td>
-                <td>${poke.total}</td>
-                <td>${poke.hp}<td>
-                <td>${poke.attack}<td>
-                <td>${poke.defense}<td>
-                <td>${poke.spatk}<td>
-                <td>${poke.spdef}<td>
-                <td>${poke.speed}<td>
-                <td>${poke.generation}<td>`),
-        )    
-    );
+d3.json("pokedex").then(pokedex=>
+    d3.json("match_vars").then(match_vars=>
+        roster_table(pokedex,match_vars)
+        )
+    )
+
+
+// d3.json("pokedex").then(pokemon=>
+//     d3.json("match_vars").then(vars=>
+//         a = vars[0].gen,
+//         console.log(a),
+//         poke = pokemon[0]["gen1"],
+//         d3.select("tbody")
+//             .selectAll("tr")
+//             .data(poke)
+//             .enter()
+//             .append("tr")
+//             .html(poke => `<td>${poke.name}</td>
+//                 <td>${poke.type1}</td>
+//                 <td>${poke.total}</td>
+//                 <td>${poke.hp}<td>
+//                 <td>${poke.attack}<td>
+//                 <td>${poke.defense}<td>
+//                 <td>${poke.spatk}<td>
+//                 <td>${poke.spdef}<td>
+//                 <td>${poke.speed}<td>
+//                 <td>${poke.generation}<td>`),
+//         )    
+//     );
 
 // d3.select("tbody")
 //   .selectAll("tr")
