cbf04c38873a24eb7407a0b582e97aad303bca07
John Whitaker <johnwhitakeraustin@gmail.com>
2020-11-22 12:04:49 -0800

    Cleaning up before moving to Match Active

--- app_poke2.py
+++ app_poke2.py
@@ -3,47 +3,37 @@ from flask_pymongo import PyMongo
 import pymongo
 import requests
 import sys
-import mongo_funz
-
+# Data Variables --------------------------------------------------
 poke2=Flask(__name__)
 poke2.config['JSON_SORT_KEYS'] = False
 poke2.config["MONGO_URI"]="mongodb://localhost:27017/pokefight2"
 mongo=PyMongo(poke2)
 pokedex = mongo.db.pokedex
 combat_vars = mongo.db.combat_vars
-match_vars = mongo.db.match_vars
-# rosters = mongo.db.rosters
-# active = mongo.db.active
-
-# TEST ROUTE
-
-
-# Frontend App Routes
+# Frontend App Routes --------------------------------------------------
+# Main Menu routes --------------------------------------------------
 @poke2.route("/")
 def index():
     return render_template(
         "index.html",
     )
-
 @poke2.route("/info_menu")
 def info_menu():
     return render_template(
         "info_menu.html",
     )
-
 @poke2.route("/pokedex")
 def servePokdex():
     return render_template(
         "pokedex.html",
         pokedex = pokedex,
     )
-
+# Match Routes --------------------------------------------------
 @poke2.route("/match_setup",methods=['GET','POST'])
 def match_setup():
     return render_template(
         "match_setup.html",
     )
-
 @poke2.route("/roster_select",methods=['GET','POST'])
 def roster_select():
     if request.method == 'POST':
@@ -51,7 +41,6 @@ def roster_select():
         npoke = request.form['npoke']
         weight_class = request.form['weight_class']
         gen = request.form['gen']
-        mongo_funz.match_vars_push(nplayers,npoke,weight_class,gen)
         return render_template(
             'roster_select.html',
             nplayers = nplayers,
@@ -60,45 +49,26 @@ def roster_select():
             gen = gen,
             pokedex = pokedex,
         )
-
 @poke2.route("/match_active",methods=['GET','POST'])
 def match_active():
     return render_template(
         'match_active.html',
         pokedex = pokedex,
     )
-
 @poke2.route("/match_over")
 def match_over():
     return render_template(
         "match_over.html",
     )
-
-# Backend App Routes
+# Backend App Routes --------------------------------------------------
 @poke2.route("/pokedex_data")
 def servePokemon():
     return jsonify(list(pokedex.find({ },
    { '_id': 0})))
-
 @poke2.route("/combat_vars")
 def serveCombatvars():
     return jsonify(list(combat_vars.find({ },
    { '_id': 0})))
-
-@poke2.route("/match_vars")
-def serveMatchvars():
-    return jsonify(list(match_vars.find({ },
-   { '_id': 0})))
-
-# @app.route("/rosters")
-# def servePokemon():
-#     return jsonify(list(rosters.find({ },
-#    { '_id': 0})))
-
-# @app.route("/active")
-# def servePokemon():
-#     return jsonify(list(active.find({ },
-#    { '_id': 0})))
-
+# Run App --------------------------------------------------
 if __name__=="__main__":
     poke2.run(debug=True)
--- mongo_funz.py
+++ mongo_funz.py
@@ -4,13 +4,6 @@ def match_vars_push(nplayers,npoke,weight_class,gen):
     conn = 'mongodb://localhost:27017'
     client = pymongo.MongoClient(conn)
     db = client.pokefight2
-    match_vars_dict = {
-        "nplayers":nplayers,
-        "npoke":npoke,
-        "weight_class":weight_class,
-        "gen":gen
-    }
     match_vars = db.match_vars
     match_vars.drop()
-    match_vars.insert_one(match_vars_dict)
 
