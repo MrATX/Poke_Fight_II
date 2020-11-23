43bf4928f1f1b0f80f514f32cc8d011ac4a15c96
Johnny Whitaker <49572101+MrATX@users.noreply.github.com>
2020-11-22 16:23:03 -0800

    Delete mongo_funz.py

--- mongo_funz.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-def match_vars_push(nplayers,npoke,weight_class,gen):
-    conn = 'mongodb://localhost:27017'
-    client = pymongo.MongoClient(conn)
-    db = client.pokefight2
-    match_vars = db.match_vars
-    match_vars.drop()
-
