60f4920796c2de72ba44a6dbdd4055c13770e2cb
John Whitaker <johnwhitakeraustin@gmail.com>
2020-11-23 07:09:17 -0800

    Got Pokedex filter working

--- static/js/pokedex.js
+++ static/js/pokedex.js
@@ -1,11 +1,7 @@
-var weight_class = document.getElementById("weight_filter").innerText
-var gen = document.getElementById("gen_filter").innerText
-
-
 function pokedex_table(pokedex){
     d3.select("tbody")
         .selectAll("tr")
-        .data(pokedex[0]["pokedex"])
+        .data(pokedex)
         .enter()
         .append("tr")
         .html(d => `<td id="poketablerow">${d.num}</td>
@@ -24,6 +20,58 @@ function pokedex_table(pokedex){
             <td id="poketablerow">${d.legendary}`)
 }
 
+function pokedex_filter(){
+    d3.json("pokedex_data").then(pokedex=>
+        pokedex_table_filtered(pokedex)
+        )
+}
+
+function pokedex_table_filtered(pokedex){
+    tbody = d3.select("tbody");
+    tbody.html("")
+    var weight_class = document.getElementById("weight_filter").value
+    var gen = document.getElementById("gen_filter").value
+    var pokedex = pokedex[0]["pokedex"]
+    let weight_class_vars = {
+        "feather":[0,250],
+        "light":[250,350],
+        "middle":[350,500],
+        "heavy":[500,1000],
+        "legendary":[0,1000],
+        "all":[0,1000]
+    }
+    var str_min = weight_class_vars[weight_class][0]
+    var str_max = weight_class_vars[weight_class][1]
+    if(gen==="all"){
+        var gen_var = [1,2,3,4,5,6]
+    }
+    if(gen!="all"){
+        var gen_var = gen
+    }
+    var legend_var = [" - "]
+    if(weight_class==="all"){
+        legend_var = ["Legendary"," - "]
+    }
+    if(weight_class==="legendary"){
+        legend_var = ["Legendary"]
+    }
+    var match_roster = []
+    var k = 0
+    pokedex.forEach(pokemon=>{
+        if(
+            pokemon.total >= str_min &&
+            pokemon.total < str_max &&
+            gen_var.includes(pokemon.generation) &&
+            legend_var.includes(pokemon.legendary)
+        ){
+            pokemon.tableindex = k
+            k = k + 1
+            match_roster.push(pokemon)
+        }
+    })
+    pokedex_table(match_roster)
+}
+
 d3.json("pokedex_data").then(pokedex=>
-    pokedex_table(pokedex)
+    pokedex_table(pokedex[0]["pokedex"])
     )
