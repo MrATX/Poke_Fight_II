4c49793b4f1208c69b35acaa271dffe308eb28cf
John Whitaker <johnwhitakeraustin@gmail.com>
2020-11-26 08:49:01 -0800

    Got Pokedex filters and check value grab. Gettin' there

--- static/js/pokedex2.js
+++ static/js/pokedex2.js
@@ -1,7 +1,6 @@
 // Base function rendering table with given list of Pokemon
 function pokedex_table(pokedex,headers){
     for(var i=0,length=headers.length;i<length;i++){
-        console.log(headers[i])
         d3.select("thead")
             .append("th")
             .text(headers[i])
@@ -96,14 +95,19 @@ function render_pokedex(){
         )
 }
 // Render Filters
-// Type 1
+// Type 1 & Type 2
 function render_type_filters(combat_vars,type_no){
     types = combat_vars[0].types
+    var title = "Type"+type_no
     selection = "type"+type_no+"_filter"
     d3.select(selection)
         .append("div")
         .attr("class","row")
         .attr("id","pokedex_filter")
+        .append("div")
+        .attr("class","container-fluid")
+        .attr("id","filter_title")
+        .text(title)
     for(var i=0,length=types.length;i<length;i++){
         var img_id = types[i]+"_type"+type_no+"_filter"
         var img_url = "static/images/type_imgs/" + types[i] + ".png"
@@ -115,9 +119,107 @@ function render_type_filters(combat_vars,type_no){
             .attr("src",img_url)
     }
 }
-// Type 2
 // Weight Class
+function render_weight_class_filter(match_vars){
+    weight_class = match_vars[0].radios.weight_class
+    d3.select("weight_class_filter")
+        .append("div")
+        .attr("class","row")
+        .attr("id","pokedex_filter")
+        .append("div")
+        .attr("class","container-fluid")
+        .attr("id","filter_title")
+        .text("Weight Class")
+    for(var i=0,length=weight_class.values.length;i<length;i++){
+        var checkbox_id = weight_class.values[i]+"_checkbox"
+        var checkbox_text = weight_class.text[i]
+        var checkbox_value = weight_class.values[i]
+        d3.select("weight_class_filter")
+            .select(".row")
+            .append("span")
+            .attr("class","checkmark")
+            .html(`<input id="${checkbox_id}" class="checkbox" type="checkbox" name="weight_class" value="${checkbox_value}>
+                    <label class="checkbox_label">${checkbox_text}</label>`)
+
+
+        //     .append("input")
+        //     .attr("id",checkbox_id)
+        //     .attr("class","checkbox")
+        //     .attr("type","checkbox")
+        //     .attr("name","weight_class")
+        //     .attr("value",checkbox_value)
+        // d3.select("weight_class_filter")
+        //     .select(".row")
+        //     .append("label")
+        //     .attr("class","pokedex_filter_label")
+        //     .text(checkbox_text)
+    }
+}
 // Generation
+function render_generation_filter(match_vars){
+    generations = match_vars[0].radios.generation
+    d3.select("generation_filter")
+        .append("div")
+        .attr("class","row")
+        .attr("id","pokedex_filter")
+        .append("div")
+        .attr("class","container-fluid")
+        .attr("id","filter_title")
+        .text("Generation")
+    for(var i=0,length=generations.values.length;i<length;i++){
+        var checkbox_id = generations.values[i]+"_checkbox"
+        var checkbox_text = generations.text[i]
+        var checkbox_value = generations.values[i]
+        d3.select("generation_filter")
+            .select(".row")
+            .append("input")
+            .attr("id",checkbox_id)
+            .attr("class","checkbox")
+            .attr("type","checkbox")
+            .attr("name","generation")
+            .attr("value",checkbox_value)
+        d3.select("generation_filter")
+            .select(".row")
+            .append("label")
+            .attr("class","pokedex_filter_label")
+            .text(checkbox_text)
+    }
+}
+//Show & Hide functions for filters
+function show_filters(){
+    d3.select("filter_buttons")
+        .style("display","inline")
+    d3.select("#showfilters")
+        .style("display","none")
+    d3.select("#hidefilters")
+        .style("display","inline")
+}
+function hide_filters(){
+    d3.select("filter_buttons")
+        .style("display","none")
+    d3.select("#showfilters")
+        .style("display","inline")
+    d3.select("#hidefilters")
+        .style("display","none")
+}
+//Variable Grabbing
+function grab_checkbox_vars(){
+    var weight_class_filter_list = []
+    var generation_filter_list = []
+    var checks = document.getElementsByClassName("checkbox")
+    for(var i=0,length=checks.length;i<length;i++){
+        if(checks[i].checked===true){
+            if(checks[i].name === "weight_class"){
+                weight_class_filter_list.push(checks[i].value)
+            }
+            if(checks[i].name === "generation"){
+                generation_filter_list.push(checks[i].value)
+            }
+        }
+    }
+    console.log(weight_class_filter_list)
+    console.log(generation_filter_list)
+}
 // Call functions to render Filters & Pokdex on page load
 render_pokedex()
 d3.json("combat_vars").then(combat_vars=>
@@ -125,4 +227,10 @@ d3.json("combat_vars").then(combat_vars=>
     )
 d3.json("combat_vars").then(combat_vars=>
     render_type_filters(combat_vars,"2")
+    )
+d3.json("match_vars").then(match_vars=>
+    render_weight_class_filter(match_vars)
+    )
+d3.json("match_vars").then(match_vars=>
+    render_generation_filter(match_vars)
     )
